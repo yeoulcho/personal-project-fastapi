d6af43ceeaa5882623c039f6c30b11ada689f151
YeoulCho <cyu5733@gmail.com>
2023-07-18 11:53:28 +0900

    feat: [조여울] 카페에 대한 네이버 블로그 크롤링 [CGF-1]

---
+++ cafe/crawl_blog_router.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+import sys
+import urllib.request
+
+import json
+import pandas as pd
+from fastapi import APIRouter
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+crawl_blog_router = APIRouter()
+
+
+def getresult(client_id, client_secret, query, display=10, start=1, sort='sim'):
+    encText = urllib.parse.quote(query)
+    url = "https://openapi.naver.com/v1/search/blog?query=" + encText + \
+          "&display" + str(display) + "&start" + str(start) + "&sort" + sort
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if (rescode == 200):
+        response_body = response.read()
+        response_json = json.loads(response_body)
+    else:
+        print("Error Code:" + rescode)
+
+    return pd.DataFrame(response_json['items'])
+
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('./naver_search_api.ini')
+client_id = config['DEFAULT']['client_id']
+client_secret = config['DEFAULT']['client_secret']
+display = 10
+start = 1
+sort = 'sim'
+result_all = pd.DataFrame()
+
+from pydantic import BaseModel
+class RequestQuery(BaseModel):
+    placeName: str
+
+
+@crawl_blog_router.get("/api/crawl-blog")
+async def crawl_blog_router1(placeName: str):
+    query = placeName
+    print(query)
+    # return result_all
+    # for i in range(0, 2):
+    #     start = 1 + 100 * i
+    result = getresult(client_id, client_secret, query, display, start, sort)
+    for i in ["title", "description"]:
+        for j in range(len(result.values)):
+            result[i][j] = result[i][j].replace("<b>", "").replace("</b>", "")
+    # print(result.replace({"<b>" : " ","</b>": " "}))
+    return result
+    # result_all = pd.concat([result_all, result])
+    # return jsonify(result_all)
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from cafe.crawl_blog_router import crawl_blog_router
+app = FastAPI()
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+origins = ["http://localhost:8080"]
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # *별 찍으면 안됨! 전부다 허용하겠다는 이야기라
+    allow_credentials=True,
+    allow_methods=["*"],  # *은 전부다
+    allow_headers=["*"],
+)
+app.include_router(crawl_blog_router)
+
